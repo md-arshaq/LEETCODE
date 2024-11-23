e0ff76c1653a07185296837b950d4f4e5e2dc5ac
Arshaq Mohammed <139967867+md-arshaq@users.noreply.github.com>
2024-11-23 16:59:57 +0530

    Time: 332 ms (5.53%) | Memory: 81.3 MB (15.81%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.py
@@ -19,21 +19,6 @@ class Solution:
             temp=temp.next
         return dummy.next
 
-        # # Dummy node to hold the result list
-        # current = ListNode() # Pointer to the current node in the result list
-        # temp = head.next  # Skip the first zero node
-        # sum1 = 0
-        
-        # while temp:
-        #     if temp.val != 0:
-        #         sum1 += temp.val
-        #     else:  # Encountered a zero, segment ends
-        #         current.next = ListNode(val=sum1)  # Add new node with the sum
-        #         current = current.next  # Move to the new node
-        #         sum1 = 0  # Reset the sum for the next segment
-        #     temp = temp.next  # Move to the next node
-        
-        # return current.next  # Return the merged result list, skipping dummy
 
                 
 
