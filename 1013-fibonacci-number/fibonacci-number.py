472dddb4026be9a5162642f060d48cb43f7ebf50
Arshaq Mohammed <139967867+md-arshaq@users.noreply.github.com>
2024-11-27 21:24:54 +0530

    Time: 34 ms (70.83%) | Memory: 17.1 MB (10.94%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -2,14 +2,25 @@ class Solution:
     def fib(self, n: int) -> int:
         #RECURSIVE APPROACH
 
-        def recursive(n):
-            if n==0 or n==1:
-                return n
-            else:
-                sum1 = recursive(n-1)+recursive(n-2)
-            return sum1
+        # def recursive(n):
+        #     if n==0 or n==1:
+        #         return n
+        #     else:
+        #         k = recursive(n-1)+recursive(n-2)
+        #     return k
         
-        return recursive(n)
+        # return recursive(n)
+
+        if n==0 or n==1:
+            return n
+        a = 0
+        b = 1
+        for i in range(2,n+1):
+            temp = a
+            a = b
+            b = temp+b
+        return b
+
 
 
 
