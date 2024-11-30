60b209f0e8bddc856660b4062b99cf8415a168d7
Arshaq Mohammed <139967867+md-arshaq@users.noreply.github.com>
2024-11-30 19:09:17 +0530

    Time: 71 ms (89.05%) | Memory: 17.2 MB (7.41%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.py
+++ 2037-count-square-sum-triples/count-square-sum-triples.py
@@ -1,33 +1,13 @@
 class Solution:
-    def binarySearch(self, nums: List[int], target: int) -> bool:
-            low, high = 0, len(nums) - 1
-            while low <= high:
-                mid = low + (high - low) // 2
-                if nums[mid] == target:
-                    return True
-                elif nums[mid] < target:
-                    low = mid + 1
-                else:
-                    high = mid - 1
-            return False
-
     def countTriples(self, n: int) -> int:
-        l1 = []
-        for i in range(1,n+1):
-            l1.append(i)
-        print(l1)
-    
         sqr = set()
-        for i in l1:
+        for i in range(n+1):
             sqr.add(i**2)
-        print(sqr)
         
         count=0
-        for i in range(len(l1)):
-            for j in range(len(l1)):
-                if i==j:
-                    continue
-                if l1[i]**2+l1[j]**2 in sqr:
+        for i in range(1,n):
+            for j in range(i+1,n):
+                if i**2+j**2 in sqr:
                     count+=1
         
-        return count
+        return count*2
