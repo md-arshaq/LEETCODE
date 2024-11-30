677eaf84d7c1bad51e384f9570530047dd1beb24
Arshaq Mohammed <139967867+md-arshaq@users.noreply.github.com>
2024-11-30 15:10:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (5.69%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -1,9 +1,22 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+
+        #BRUTEST FORCEST
+
+        # count=0
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         for k in range(j+1,len(nums)):
+        #             if nums[j] - nums[i] == diff and nums[k] - nums[j] == diff:
+        #                 count+=1
+        # return count
+
+        # USING HASH SET
+        
         count=0
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                for k in range(j+1,len(nums)):
-                    if nums[j] - nums[i] == diff and nums[k] - nums[j] == diff:
-                        count+=1
+        hashset = set(nums)
+        for i in nums:
+            if (i+diff) in hashset and (i+2*diff) in hashset:
+                count+=1
         return count
+
