010f26017494e9c37bedcdc965867879de768051
Arshaq Mohammed <139967867+md-arshaq@users.noreply.github.com>
2024-12-03 19:59:51 +0530

    Time: 78 ms (68.51%) | Memory: 48.9 MB (44.10%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -1,13 +1,32 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
+
+        # INEFFICIENT APPROACH 
+        # spaces.append(0)
+        # j=0
+        # res = ""
+        # for i in range(len(s)):
+        #     if i!=spaces[j]:
+        #         res+=s[i]
+        #     elif i==spaces[j]:
+        #         res+=" "
+        #         res+=s[i]
+        #         j+=1
+        # return res
+
+        res = []
+
         spaces.append(0)
         j=0
-        res = ""
-        for i in range(len(s)):
-            if i!=spaces[j]:
-                res+=s[i]
-            elif i==spaces[j]:
-                res+=" "
-                res+=s[i]
+        count=0
+        for i in s:
+            if count!=spaces[j]:
+                res.append(i)
+                count+=1
+            else:
+                res.append(" ")
+                res.append(i)
                 j+=1
-        return res
+                count+=1
+        return "".join(res)
+
