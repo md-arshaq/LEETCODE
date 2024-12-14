eefce39e4af5f874be1213844ca2caf686ac58ee
Arshaq Mohammed <139967867+md-arshaq@users.noreply.github.com>
2024-12-15 01:55:43 +0530

    Time: 2 ms (21.69%) | Memory: 17.5 MB (6.86%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,14 +1,37 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        res = len(students)
 
-        count = Counter(students)
-        print(count)
+        # THE OPTIMAL ONE 
+        # # res = len(students)
 
-        for i in sandwiches:
-            if count[i]>0:
-                res-=1
-                count[i]-=1
+        # # count = Counter(students)
+        # # print(count)
+
+        # # for i in sandwiches:
+        # #     if count[i]>0:
+        # #         res-=1
+        # #         count[i]-=1
+        # #     else:
+        # #         return res
+        # # return res
+
+        while(students):
+            if students[0]==sandwiches[0]:
+                students.pop(0)
+                sandwiches.pop(0)
             else:
-                return res
-        return res
+                x = students.pop(0)
+                students.append(x)
+
+            all_dislike = True
+
+            for student in students:
+                if student == sandwiches[0]:
+                    all_dislike = False
+                    break
+
+            if all_dislike:
+                break
+
+        return len(sandwiches)
+
