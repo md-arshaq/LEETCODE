b4ba2b15d0cdeb5c5a9b192cc35d4ea250a177d2
Arshaq Mohammed <139967867+md-arshaq@users.noreply.github.com>
2024-12-16 19:57:37 +0530

    Time: 3 ms (36.98%) | Memory: 17.4 MB (13.21%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,19 +1,20 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)-1):
+        
+        n = len(nums)
+
+        for i in range(n-1):
             if nums[i]==nums[i+1]:
                 nums[i]*=2
                 nums[i+1]=0
-        print(nums)
 
         j=0
-        for i in range(len(nums)):
+        for i in range(n):
             if nums[i]!=0:
                 nums[j]=nums[i]
                 j+=1
-        print(nums)
 
-        while(j<len(nums)):
+        while(j<n):
             nums[j]=0
             j+=1
         
